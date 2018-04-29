44b844932d29b6e6832063e6f0089495784bfe67
Stanislav Mosienko <stanislav.dev@gmail.com>
2018-04-29 11:44:41 +0200

    Initial Commit

---
+++ SQLite/SQLite.h
@@ -0,0 +1,19 @@
+//
+//  SQLite.h
+//  SQLite
+//
+//  Created by Stanislav Antonov on 29/04/2018.
+//  Copyright © 2018 Practical Software Engineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SQLite.
+FOUNDATION_EXPORT double SQLiteVersionNumber;
+
+//! Project version string for SQLite.
+FOUNDATION_EXPORT const unsigned char SQLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SQLite/PublicHeader.h>
+
+
